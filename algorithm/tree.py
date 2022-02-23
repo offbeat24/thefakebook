d1beec21ce851fef263d9ae28bebc6c3ef25eebe
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2022-02-24 00:09:20 +0900

    Update tree.py

--- algorithm/tree.py
+++ algorithm/tree.py
@@ -2,10 +2,11 @@ from collections import deque
 
 
 class Node:
-    def __init__(self, item) :
+    def __init__(self, item):
         self.item = item
         self.left = None
         self.right = None
+
 
 n1 = Node(10)
 n2 = Node(20)
@@ -16,9 +17,11 @@ n6 = Node(60)
 n7 = Node(70)
 n8 = Node(80)
 
+
 class BinaryTree():
     def __init__(self):
         self.root = None
+
 
 tree = BinaryTree()
 tree.root = n1
@@ -30,23 +33,29 @@ n3.left = n6
 n3.right = n7
 n4.left = n8
 
-#전위 순회 
+# 전위 순회
+
+
 def preorder(self, n):
     if n != None:
-        print(n.item, "", end = '')
+        print(n.item, "", end='')
         if n.left:
             self.preorder(n.left)
         if n.right:
             self.preorder(n.right)
-#후위순회          
+# 후위순회
+
+
 def postorder(self, n):
     if n != None:
         if n.left:
             self.postorder(n.left)
         if n.right:
             self.postorder(n.right)
-        print(n.item, "", end = '')
-#중위 순회
+        print(n.item, "", end='')
+# 중위 순회
+
+
 def inorder(self, n):
     if n != None:
         if n.left:
@@ -54,15 +63,4 @@ def inorder(self, n):
         print(n.item, "", end="")
         if n.right:
             self.inorder(n.right)
-#레벨 순회
-def levelorder(self,n):
-    q=deque()
-    q.append(n)
-    while q:
-        t = q.popleft()
-        print(t.item, "", end="")
-        if t.left != None:
-            q.append(t.left)
-        if t.right != None:
-            q.append(t.right)
-        
+# 레벨 순회
