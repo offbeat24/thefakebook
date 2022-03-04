54eed58d74592d66ca7d6d1a0fe44c0ccee82615
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2022-03-05 03:07:08 +0900

    py

--- python/6416.py
+++ python/6416.py
@@ -10,38 +10,6 @@ class Node:
     def __str__(self):
         return str(self.data)
 
-class binary_tree() :
-    def __init__(self) :
-        self.root = None
-    
-def find_child(node) :
-    global node_arr
-    child_node_arr = node.child
-    children = deque()
-    for i in child_node_arr :
-        for key, value in node_arr.items() :
-            if value == i :
-                children.append(key)
-                break
-            
-        
-    return children
-        
-        
-
-def dfs(start_num) :
-        visited = list()
-        need_visited = deque()
-        need_visited.append(start_num)
-        global node_arr
-        while need_visited :
-            start_node = need_visited.popleft()
-            if start_node not in visited :
-                visited.append(start_node)
-                need_visited.extend(find_child(node_arr[start_node]))
-                
-        return visited
-        
 
 input_case = list()
 case = deque()
@@ -70,16 +38,13 @@ def tree_pot():
 
 
 tree_pot()
-case_cnt = 0
 
 while case:
     test_case = case.popleft()
+    print(test_case)
     if test_case == deque([0, 0]):
         answer.append(True)
         continue
-    else :
-        test_case.pop()
-        test_case.pop()
     test_case_set = set(test_case)
     node_arr = dict()
     for i in test_case_set:
@@ -103,19 +68,8 @@ while case:
             tmp.append(num)
     if len(tmp) != 1:
         answer.append(False)
-        continue
     else:
-        tree_ = binary_tree()
-        r = tmp.pop()
-        tree_.root = node_arr[r]
-        visited = dfs(r)
-        for i in test_case_set :
-            if i not in visited :
-                answer.append(False)
-                break
-        else:
-            answer.append(True)
-        
+        answer.append(True)
 
 
 for i in range(len(answer)):
