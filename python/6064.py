7e8cd4adf4fdb862a564cefcd9e6d53d7ff28461
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2023-06-14 23:45:30 +0900

    Create 6064.py

---
+++ python/6064.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for _ in range(t) :
+  m, n, x, y = map(int, input().split())
+  
+  while(x <= m*n) :
+    if x % n == y % n :
+      print(x)
+      break
+    x += m
+  else :
+    print(-1)
